9cc6fee6ee6e3a551062e474bb10234e00f14be6
Daniel Leong <danielleong47@gmail.com>
2025-02-17 15:36:55 +0800

    Refactored project structure by moving directories into src/

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+        <nav>
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Projects</h1>
+            <h1>Contact Me</h1>
+        </nav>
+    );
+
+}
+
+export default Navbar;
